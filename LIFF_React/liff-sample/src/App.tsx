fa6762a01973c1e9e6956d9740c02c70cac640ad
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2021-03-03 21:00:26 +0900

    [edit]¥だったので、\に改行文字を変更

--- LIFF_React/liff-sample/src/App.tsx
+++ LIFF_React/liff-sample/src/App.tsx
@@ -41,7 +41,7 @@ function App() {
               const userId: string = profile.userId
               const displayName: string = profile.displayName
               const screenWhere=liff.getContext();
-              window.alert(`Name: ${displayName} ¥n userId: ${userId} ¥n screenWhere:${screenWhere}`);
+              alert(`Name: ${displayName}\n userId: ${userId}\n screenWhere:${screenWhere}`);
               
             }).catch(function(error) {
               window.alert('Error sending message: ' + error);
